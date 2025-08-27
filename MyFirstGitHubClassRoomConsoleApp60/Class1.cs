3ccc41b35b4e69ca3967e3cd0285ac873fc8e283
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-08-27 11:04:03 +0200

    igen DemoTry klasse

---
+++ MyFirstGitHubClassRoomConsoleApp60/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class DemoTry
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+	public string Email { get; set; }
+
+
+	public DemoTry(string name, int age, string email)
+	{
+		Name = name;
+		Age = age;
+		Email = email;
+	}
+}
+
