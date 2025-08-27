5ce01440a8d97526636ccf62b6772768a0ffa200
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-08-27 11:28:30 +0200

    nu virker den

--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -3,4 +3,5 @@ using static System.Formats.Asn1.AsnWriter;
 using System.Xml.Linq;
 Console.WriteLine("Hello, World!");
 
-DemoTry person = new() { Name = "Tobias", Age = 24 };
+DemoTry person = new DemoTry{ Name = "Tobias", Age = 24, Email = "123@456.dk" };
+Console.WriteLine(person);
