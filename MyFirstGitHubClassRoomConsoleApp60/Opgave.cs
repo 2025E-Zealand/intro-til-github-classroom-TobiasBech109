162e4c04c865c07205bb0f2b049fcd583ba84b7c
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-08-27 11:33:59 +0200

    Nu med en ny klasse og efter at have forket den

---
+++ MyFirstGitHubClassRoomConsoleApp60/Opgave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Opgave
+{
+	public string Navn { get; set; }
+	public int Tid { get; set; }
+	public string Beskrivelse { get; set; }
+
+	public Opgave(string navn, int tid, string beskrivelse)
+	{
+		Navn = navn;
+		Tid = tid;
+		Beskrivelse = beskrivelse;
+	}
+
+	public Opgave()
+	{
+	}
+
+	public override string ToString()
+	{
+		return $"Navn: {Navn}, Tid: {Tid}, Beskrivelse: {Beskrivelse}";
+	}
+
+}
+
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using static System.Formats.Asn1.AsnWriter;
 using System.Xml.Linq;
-Console.WriteLine("Hello, World!");
 
-DemoTry person = new DemoTry{ Name = "Tobias", Age = 24, Email = "123@456.dk" };
+DemoTry person = new DemoTry{ Name = "Henning", Age = 72, Email = "123@456.dk" };
 Console.WriteLine(person);
+
+Console.WriteLine();
+
+Opgave opgave = new Opgave { Navn = "Classroom", Tid = 12, Beskrivelse = "Lav en klasse og prøv at fork" };
+Console.WriteLine(opgave);
